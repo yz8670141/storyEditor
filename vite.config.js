35d4f9e582e143070fed7ac8d41d19e75219eee6
yz8670141 <yz8670141@gmail.com>
2025-06-11 10:32:07 +0800

    chore: modify base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -22,14 +22,14 @@ export default defineConfig({
       }
     }
   },
-  server: {
-    host: true,
-    port: 5173,
-    watch: {
-      usePolling: true, 
-      interval: 100
-    },
+  // server: {
+  //   host: true,
+  //   port: 5173,
+  //   watch: {
+  //     usePolling: true, 
+  //     interval: 100
+  //   },
     
-  },
+  // },
   
 });
